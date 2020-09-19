af2784f9814a6d6716f183614506c2a4948846d4
MamzelNyf <fanny@shedesign.dev>
2020-09-19 12:26:32 +0800

    update reference to pledge in event

--- cauz/events/serializers.py
+++ cauz/events/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 from .models import Event, Pledge, Category, Region
 
 
-
 class CategorySerializer(serializers.Serializer):
     id = serializers.ReadOnlyField()
     name = serializers.CharField(max_length=32)
@@ -12,10 +11,11 @@ class CategorySerializer(serializers.Serializer):
         return Category.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.slug = validated_data.get('slug', instance.slug)
+        instance.name = validated_data.get("name", instance.name)
+        instance.slug = validated_data.get("slug", instance.slug)
         instance.save()
         return instance
+
 
 class RegionSerializer(serializers.Serializer):
     id = serializers.ReadOnlyField()
@@ -26,10 +26,11 @@ class RegionSerializer(serializers.Serializer):
         return Region.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.slug = validated_data.get('slug', instance.slug)
+        instance.name = validated_data.get("name", instance.name)
+        instance.slug = validated_data.get("slug", instance.slug)
         instance.save()
         return instance
+
 
 class EventSerializer(serializers.Serializer):
     id = serializers.ReadOnlyField()
@@ -38,54 +39,62 @@ class EventSerializer(serializers.Serializer):
     goal = serializers.IntegerField()
     image = serializers.URLField()
     is_open = serializers.BooleanField()
-    date_created = serializers.DateTimeField()     
+    date_created = serializers.DateTimeField()
     slug = serializers.CharField(required=False)
     # owner = serializers.CharField(max_length=200)
-    owner = serializers.ReadOnlyField(source='owner.id')
-    category = serializers.ReadOnlyField(source='Category', default='Charity')
-    region = serializers.ReadOnlyField(source='Region', default='World')
+    owner = serializers.ReadOnlyField(source="owner.id")
+    category = serializers.ReadOnlyField(source="Category", default="Charity")
+    region = serializers.ReadOnlyField(source="Region", default="World")
 
     def create(self, validated_data):
         return Event.objects.create(**validated_data)
 
+
 class PledgeSerializer(serializers.Serializer):
-    id= serializers.ReadOnlyField()
+    id = serializers.ReadOnlyField()
     amount = serializers.IntegerField()
     comment = serializers.CharField(max_length=200)
     anonymous = serializers.BooleanField()
-    supporter = serializers.ReadOnlyField(source='supporter.id')
-    event = serializers.PrimaryKeyRelatedField(source='event.id',queryset=Event.objects.all())
+    supporter = serializers.ReadOnlyField(source="supporter.id")
+    event = serializers.PrimaryKeyRelatedField(
+        source="event.id", queryset=Event.objects.all()
+    )
+    print("Pledge serialiser")
 
     def create(self, validated_data):
         print(validated_data)
         # event=Event.objects.get(pk=validated_data['event'].id)
-        
+
         return Pledge.objects.create(
-            event=validated_data['event']['id'],
-            amount=validated_data['amount'],
-            comment = validated_data['comment'],
-            anonymous=validated_data['anonymous'],
-            supporter=validated_data['supporter']
-        ) 
+            event=validated_data["event"]["id"],
+            amount=validated_data["amount"],
+            comment=validated_data["comment"],
+            anonymous=validated_data["anonymous"],
+            supporter=validated_data["supporter"],
+        )
 
     def update(self, instance, validated_data):
-        instance.amount = validated_data.get('amount', instance.amount)
-        instance.comment = validated_data.get('comment', instance.comment)
-        instance.anonymous = validated_data.get('anonymous', instance.anonymous)
+        instance.amount = validated_data.get("amount", instance.amount)
+        instance.comment = validated_data.get("comment", instance.comment)
+        instance.anonymous = validated_data.get("anonymous", instance.anonymous)
         instance.save()
         return instance
 
+
 class EventDetailSerializer(EventSerializer):
-    pledges = PledgeSerializer(many=True, read_only=True)
+    pledges = PledgeSerializer(many=True, read_only=True, source="event_pledge")
+    print("event serialiser")
 
     def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.description = validated_data.get('description', instance.description)
-        instance.goal = validated_data.get('goal', instance.goal)
-        instance.image = validated_data.get('image', instance.image)
-        instance.is_open = validated_data.get('is_open', instance.is_open)
-        instance.date_created = validated_data.get('date_created', instance.date_created)
-        instance.owner = validated_data.get('owner', instance.owner)
-        instance.category = validated_data.get('category', instance.category)
+        instance.title = validated_data.get("title", instance.title)
+        instance.description = validated_data.get("description", instance.description)
+        instance.goal = validated_data.get("goal", instance.goal)
+        instance.image = validated_data.get("image", instance.image)
+        instance.is_open = validated_data.get("is_open", instance.is_open)
+        instance.date_created = validated_data.get(
+            "date_created", instance.date_created
+        )
+        instance.owner = validated_data.get("owner", instance.owner)
+        instance.category = validated_data.get("category", instance.category)
         instance.save()
         return instance
--- cauz/events/views.py
+++ cauz/events/views.py
@@ -1,46 +1,42 @@
 from django.http import Http404
-from rest_framework import status, permissions, filters,generics
+from rest_framework import status, permissions, filters, generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Event, Pledge, Category, Region
-from .serializers import EventSerializer, PledgeSerializer, EventDetailSerializer, CategorySerializer, RegionSerializer
+from .serializers import (
+    EventSerializer,
+    PledgeSerializer,
+    EventDetailSerializer,
+    CategorySerializer,
+    RegionSerializer,
+)
 from .permissions import IsOwnerOrReadOnly, IsSupporterOrReadOnly
 from rest_framework.generics import ListAPIView
-
-
-
 
 
 class EventList(ListAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = EventSerializer
-    filter_backends = (filters.OrderingFilter, )
-    ordering_fields = ['date_created']
+    filter_backends = (filters.OrderingFilter,)
+    ordering_fields = ["date_created"]
 
     def get_queryset(self):
         queryset = Event.objects.all()
-        category = self.request.query_params.get('category__name', None)
+        category = self.request.query_params.get("category__name", None)
         if category is not None:
             queryset = queryset.filter(category=category)
         return queryset
 
-    def post(self,request):
+    def post(self, request):
         serializer = EventSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save(owner=request.user)
-            return Response(
-            serializer.data,
-            status=status.HTTP_201_CREATED
-            )
-        return Response(
-            serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST
-        )
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class EventDetail(APIView):
-    permission_classes = [
-        permissions.IsAuthenticatedOrReadOnly,
-        IsOwnerOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
     def get_object(self, slug):
         try:
@@ -51,59 +47,47 @@ class EventDetail(APIView):
             raise Http404
 
     def get(self, request, slug):
+
         event = self.get_object(slug)
+        print(event)
         serializer = EventDetailSerializer(event)
         return Response(serializer.data)
 
     def put(self, request, slug):
         event = self.get_object(slug)
         serializer = EventDetailSerializer(
-            instance = event,
-            data=request.data,
-            partial=True
+            instance=event, data=request.data, partial=True
         )
         if serializer.is_valid():
             serializer.save(owner=request.user)
-            return Response(
-            serializer.data,
-            status=status.HTTP_200_OK
-            )
-        return Response(
-            serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST
-        )
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, slug):
-            event = self.get_object(slug)
-            event.delete()
-            return Response(status=status.HTTP_200_OK)
+        event = self.get_object(slug)
+        event.delete()
+        return Response(status=status.HTTP_200_OK)
+
 
 class PledgeList(APIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    
+
     def get(self, request):
         pledges = Pledge.objects.all()
         serializer = PledgeSerializer(pledges, many=True)
         return Response(serializer.data)
 
-    def post(self,request):
+    def post(self, request):
         print(request.data)
         serializer = PledgeSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save(supporter=request.user)
-            return Response(
-            serializer.data,
-            status=status.HTTP_201_CREATED
-            )
-        return Response(
-            serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST
-        )
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class PledgeDetail(APIView):
-    permission_classes = [
-        permissions.IsAuthenticatedOrReadOnly,
-        IsSupporterOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsSupporterOrReadOnly]
 
     def get_object(self, pk):
         try:
@@ -120,50 +104,39 @@ class PledgeDetail(APIView):
 
     def put(self, request, pk):
         pledge = self.get_object(pk)
-        serializer = PledgeSerializer(
-            instance = pledge,
-            data=request.data,
-            partial=True
-        )
+        serializer = PledgeSerializer(instance=pledge, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save(supporter=request.user)
-            return Response(
-            serializer.data,
-            status=status.HTTP_200_OK
-            )
-        return Response(
-            serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST
-        )
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
-            pledge = self.get_object(pk)
-            pledge.delete()
-            return Response(status=status.HTTP_200_OK)
+        pledge = self.get_object(pk)
+        pledge.delete()
+        return Response(status=status.HTTP_200_OK)
+
 
 class CategoryList(APIView):
-    permission_classes = [ permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
     def get(self, request):
         categories = Category.objects.all()
         serializer = CategorySerializer(categories, many=True)
         return Response(serializer.data)
 
-    
-    def post(self,request):
+    def post(self, request):
         serializer = CategorySerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(
-            serializer.data,
-            status=status.HTTP_201_CREATED
-            )
-        return Response(
-            serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST
-        )
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class CategoryDetail(APIView):
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly,permissions.IsAdminUser]
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly,
+        permissions.IsAdminUser,
+    ]
 
     def get_object(self, slug):
         try:
@@ -181,48 +154,39 @@ class CategoryDetail(APIView):
     def put(self, request, slug):
         category = self.get_object(slug)
         serializer = CategorySerializer(
-            instance = category,
-            data=request.data,
-            partial=True
+            instance=category, data=request.data, partial=True
         )
         if serializer.is_valid():
-            return Response(
-            serializer.data,
-            status=status.HTTP_200_OK
-            )
-        return Response(
-            serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST
-        )
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, slug):
-            category = self.get_object(slug)
-            category.delete()
-            return Response(status=status.HTTP_200_OK)
+        category = self.get_object(slug)
+        category.delete()
+        return Response(status=status.HTTP_200_OK)
+
 
 class RegionList(APIView):
-    permission_classes = [ permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def get(self, request):
         regions = Region.objects.all()
         serializer = RegionSerializer(regions, many=True)
         return Response(serializer.data)
 
-    def post(self,request):
+    def post(self, request):
         serializer = RegionSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(
-            serializer.data,
-            status=status.HTTP_201_CREATED
-            )
-        return Response(
-            serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST
-        )
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class RegionDetail(APIView):
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly,permissions.IsAdminUser]
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly,
+        permissions.IsAdminUser,
+    ]
 
     def get_object(self, slug):
         try:
@@ -239,20 +203,10 @@ class RegionDetail(APIView):
 
     def put(self, request, slug):
         region = self.get_object(slug)
-        serializer = RegionSerializer(
-            instance = region,
-            data=request.data,
-            partial=True
-        )
+        serializer = RegionSerializer(instance=region, data=request.data, partial=True)
         if serializer.is_valid():
-            return Response(
-            serializer.data,
-            status=status.HTTP_200_OK
-            )
-        return Response(
-            serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST
-        )
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, slug):
         region = self.get_object(slug)
